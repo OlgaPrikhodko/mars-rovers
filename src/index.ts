49f4193139b2b320169b7a51c16aed7cf57bce97
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-01-11 16:46:51 +0200

    add types and functionality base

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 // Lines of Input to the Program:
-
 // 5 5         // Plateau
-
 // 1 2 N       // Rover Position
-
 // LMLMLMLMM   // Instructions
 
 // Expected Output:
@@ -12,3 +9,67 @@
 // plateau = createPlateau(x: number, y: number)
 // rover: Rover = createRover(pos: Position)
 // newPos:Position = setInstruction(rover: Rover, instructions: string)
+
+// TYPES:
+const directions = ["N", "E", "S", "W"] as const;
+type Direction = typeof directions[number];
+
+const possibleInstructions = ["L", "R", "M"] as const;
+type InstructionsType = typeof possibleInstructions[number];
+
+interface Position {
+  x: number;
+  y: number;
+  direction: Direction;
+}
+
+interface Rover {
+  pos: Position;
+}
+
+// MAIN
+function createPos(x: number, y: number, direction: Direction): Position {
+  return { x, y, direction };
+}
+
+let pos = createPos(1, 2, "N");
+const instructions = getInstructions("LMLMLMLMM");
+
+function getInstructions(s: string): InstructionsType[] {
+  const res: InstructionsType[] = [];
+
+  for (let letter of s) {
+    if (letter === "L" || letter === "R" || letter === "M") {
+      res.push(letter);
+    } else throw new Error("wrong instructions");
+  }
+
+  return res;
+}
+
+const newPos = setInstructions(pos, instructions);
+
+function setInstructions(pos: Position, instructions: InstructionsType[]) {
+  for (let instruction of instructions) {
+    pos = setInstruction(pos, instruction);
+  }
+
+  return pos;
+}
+
+function setInstruction(pos: Position, instruction: string): Position {
+  if (instruction === "M") {
+    return moveRover(pos);
+  }
+
+  return changeDirection(pos);
+}
+
+// ROVER ACTIONS
+function moveRover(pos: Position): Position {
+  return pos;
+}
+
+function changeDirection(pos: Position): Position {
+  return pos;
+}
